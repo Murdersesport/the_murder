07ef12a412be13a366d32fdbf9981488a4c54cb3
Murdersesport <murdersesports@gmail.com>
2022-06-27 02:16:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const Client = require("discord.js")
 
-bot.on("ready", async () => {
 
-    console.log(`${bot.user.username} : Onligne in ${bot.guilds.cahes.size} server`)
-})
 
 Client.start(process.env.token)

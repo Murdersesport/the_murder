26ebb299b3c290091fb4ba566b4340477b1ed1d1
Murdersesport <murdersesports@gmail.com>
2022-06-29 20:10:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ Client.on("messageCreate", message => {
 Client.on("interactionCreate", interaction =>{
       if(interaction.isButton()){
         if(interaction.customId === "Event"){
-          interaction.author.reply("kayn asat good luck trb7")
+          message.author.send("hello word")("kayn asat good luck trb7")
         }
       }
     })
